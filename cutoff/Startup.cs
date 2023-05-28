66a11994d4d8b8ab4c33118ac7d32a5a57f2569a
Mahima Chacko <mahimamchacko@gmail.com>
2023-05-28 16:04:39 -0400

    Create Startup.cs

---
+++ cutoff/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using cutoff.Helpers;
+
+namespace cutoff;
+
+public class Startup
+{
+	public IConfiguration Configuration { get; set; }
+
+	public Startup(IConfiguration configuration)
+	{
+		Configuration = configuration;
+	}
+
+	public void ConfigureServices(IServiceCollection services)
+	{
+        services.AddControllersWithViews();
+
+        services.AddRazorPages();
+
+		services.AddScoped<DataContext>();
+	}
+
+	public void Configure(WebApplication app, IWebHostEnvironment env)
+	{
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}"
+        );
+
+        app.Run();
+    }
+}
